5405e089f9dc5e609cbbe5d905622029b763e600
beom.93 <beom.93@kakaomobility.com>
2022-11-05 16:18:23 +0900

    gl initialize

---
+++ OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by beom.93 on 2022/11/05.
+//
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    GLFWwindow* window;
+    
+
+}
